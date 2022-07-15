52a02410a4f5bbc220918a34151dbeda921e29a9
Svibex <future.science@yandex.ru>
2022-07-15 11:34:14 +0300

    feat: refactor burger menu

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 let menuBtn = document.querySelector('.header_menu-btn');
 let menu = document.querySelector('.menu');
+let body = document.querySelector('body');
+let ul = document.querySelector('ul');
 menuBtn.addEventListener('click', function(){
     menuBtn.classList.toggle('active');
     menu.classList.toggle('active');
-    document.querySelector('body').classList.toggle('isVisible')
+    body.classList.toggle('isVisible')
+})
+
+ul.addEventListener('click', function(){
+    body.classList.remove('isVisible');
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
 })
